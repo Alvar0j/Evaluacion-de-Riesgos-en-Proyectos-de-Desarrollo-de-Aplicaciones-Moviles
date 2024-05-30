243bedbd2dfa5c89afd5f0ad6fd74658569317e8
Álvaro Jiménez <alvarojimenezosuna03@gmail.com>
2024-05-30 18:36:42 +0200

    Initial Commit

---
+++ PruebaLecturaCSV.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def read_csv(file_path):import pandas as pd
+
+def read_csv(file_path):
+    # Lista de países de la Unión Europea
+    eu_countries = ['Austria', 'Belgium', 'Bulgaria', 'Croatia', 'Republic of Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Germany', 'Greece', 'Hungary', 'Ireland', 'Italy', 'Latvia', 'Lithuania', 'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia', 'Spain', 'Sweden']
+
+    data = pd.read_csv(file_path)
+
+    # Filtrar los datos para incluir solo los países de la Unión Europea
+    eu_data = data[data['Country Name'].isin(eu_countries)]
+
+    print(eu_data)
+
+# Uso de la función
+file_path = '/Users/pruebaprueba/Desktop/P_Data_Extract_From_World_Development_Indicators/ba49add3-1ef6-4d95-9e32-21d877473b59_Data.csv'
+read_csv(file_path)
+data = pd.read_csv(file_path)
+print(data)
+
+# Uso de la función
+file_path = '/Users/pruebaprueba/Desktop/P_Data_Extract_From_World_Development_Indicators/ba49add3-1ef6-4d95-9e32-21d877473b59_Data.csv'
+read_csv(file_path)
